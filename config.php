655f7a4a36b5d176f8158eb1fa41e5beb67e1ee2
Marta2852 <ipa23.m.dzerve@vtdt.edu.lv>
2025-03-26 10:15:22 +0200

    Upload files

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = "localhost";
+$dbname = "upload";
+$username = "root";
+$password = "root";
+
+try{
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Datebase connected successfully!<br>";
+} catch (PDOException $e) {
+
+    die("Database connection failed: " . $e->getMessage());
+    
+}
